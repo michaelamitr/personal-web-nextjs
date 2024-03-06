91a5789676d3281dddd099e1f2b7ad75b1f29304
Michaela Trčková <misa.trckova@volny.cz>
2024-03-06 12:37:31 +0100

    Header icons update, open/clole logic, styles improvements of header and nav

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,3 +1,92 @@
+'use client';
+import styles from './header.module.css';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+import { useState } from 'react';
+
 export default function Header() {
-  return <header>hello</header>;
+  const [openBurgerMenu, setOpenBurgerMenu] = useState(false);
+
+  const pathname = usePathname();
+
+  const handleMenuClick = () => {
+    setOpenBurgerMenu(!openBurgerMenu);
+  };
+  return (
+    <header className={styles.header}>
+      <div className={styles.burgermenu_wrapper} onClick={handleMenuClick}>
+        <Image
+          width={100}
+          height={100}
+          src={openBurgerMenu ? '/close.svg' : '/burgermenuOpen.svg'}
+          alt="Menu"
+        />
+      </div>
+      {openBurgerMenu && (
+        <nav className={styles.nav} onClick={handleMenuClick}>
+          <li className={styles.header_link_item}>
+            <Link
+              href="/"
+              className={`${styles.header_link} ${
+                pathname === '/' ? styles.active : ''
+              }`}
+            >
+              Home
+            </Link>
+          </li>
+          <li className={styles.header_link_item}>
+            <Link
+              href="/about"
+              className={`${styles.header_link} ${
+                pathname === '/about' ? styles.active : ''
+              }`}
+            >
+              About
+            </Link>
+          </li>
+          <li className={styles.header_link_item}>
+            <Link
+              href="/portfolio"
+              className={`${styles.header_link} ${
+                pathname === '/portfolio' ? styles.active : ''
+              }`}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className={styles.header_link_item}>
+            <Link
+              href="/contact"
+              className={`${styles.header_link} ${
+                pathname === '/contact' ? styles.active : ''
+              }`}
+            >
+              Contact
+            </Link>
+          </li>
+          <li className={styles.header_link_item}>
+            <Link
+              href="/references"
+              className={`${styles.header_link} ${
+                pathname === '/references' ? styles.active : ''
+              }`}
+            >
+              References
+            </Link>
+          </li>
+          <li className={styles.header_link_item}>
+            <Link
+              href="/skills"
+              className={`${styles.header_link} ${
+                pathname === '/skills' ? styles.active : ''
+              }`}
+            >
+              Skills
+            </Link>
+          </li>
+        </nav>
+      )}
+    </header>
+  );
 }
