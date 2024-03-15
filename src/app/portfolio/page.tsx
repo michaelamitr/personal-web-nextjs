788dbde46d98ed773299500b28a61c9f46866b92
Michaela Trčková <misa.trckova@volny.cz>
2024-03-15 22:05:16 +0100

    styled portfolio item component

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -6,16 +6,18 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio</h1>
-      {projectsData.map((project) => (
-        <PortfolioItem
-          slug={project.slug}
-          imgUrl={project.smallImage}
-          name={project.name}
-          year={project.year}
-          shortDescription={project.shortDescription}
-          Technologies={project.technologies}
-        />
-      ))}
+      <div className={styles.portfolio_container}>
+        {projectsData.map((project) => (
+          <PortfolioItem
+            slug={project.slug}
+            imgUrl={project.smallImage}
+            name={project.name}
+            year={project.year}
+            shortDescription={project.shortDescription}
+            Technologies={project.technologies}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/PortfolioItem/portfolioItem.tsx
+++ src/components/PortfolioItem/portfolioItem.tsx
@@ -1,4 +1,5 @@
 import styles from './portfolioItem.module.css';
+import cn from 'classnames';
 import Image from 'next/image';
 import Tag from '../Tag/tag';
 import { PortfolioItem } from '@/data/interfaces';
@@ -21,14 +22,17 @@ export default function PortfolioItem({
           className={styles.project_image}
         />
       </div>
-      <div>
-        <h2>{name}</h2>
-        <Tag text={year} />
+      <div className={styles.info_container}>
+        <div className={styles.headline_container}>
+          <h2 className={styles.project_name}>{name}</h2>
+          <Tag text={year} />
+        </div>
         <p>{shortDescription}</p>
-        <div className={styles.technologies_container}></div>
-        {Technologies.map((technology) => (
-          <Tag text={technology} />
-        ))}
+        <div className={styles.technologies_container}>
+          {Technologies.map((technology) => (
+            <Tag text={technology} />
+          ))}
+        </div>
       </div>
     </div>
   );
