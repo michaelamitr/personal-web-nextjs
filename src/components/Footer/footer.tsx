44172c7345f71df1d1dbefc65c947a91cfbe312d
Michaela Trčková <misa.trckova@volny.cz>
2024-04-09 11:12:42 +0200

    styled footer responsive

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,13 +1,27 @@
 import styles from './footer.module.css';
+import Image from 'next/image';
 import ContactForm from '../ContactForm/contactForm';
 import CV from '../CV/cv';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <ContactForm />
-      <div className={styles.footer_info_container}>
-        <CV />
+      <div className={styles.footer_content}>
+        <ContactForm />
+        <div className={styles.info_wrapper}>
+          <CV />
+          <div className={styles.author_container}>
+            <h3>created by</h3>
+            <Image
+              src={'/img/signature-MT.png'}
+              alt="Michaela Trčková Signature"
+              width={949}
+              height={339}
+              className={styles.signature}
+            />
+            <h3>2024</h3>
+          </div>
+        </div>
       </div>
     </footer>
   );
